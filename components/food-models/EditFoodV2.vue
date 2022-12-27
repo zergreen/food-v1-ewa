761db38657b9456107a32981fa10aff9cb50914f
zergreen <kawhao101@gmail.com>
2022-12-27 23:59:11 +0700

    table: complete

--- components/food-models/EditFoodV2.vue
+++ components/food-models/EditFoodV2.vue
@@ -1,38 +1,47 @@
 <template>
-  <div>
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="container">
+    <div>
+      <!-- Value: '{{this.models}}' -->
+      <div class="input-group flex-nowrap">
+        <!-- <span class="input-group-text" id="addon-wrapping">@</span> -->
+        <input
+          type="text"
+          class="form-control"
+          placeholder="id"
+          aria-label="id"
+          aria-describedby="addon-wrapping"
+          v-model="id"
+        />
+      </div>
+    </div>
+    <div class="table-responsive">
+      <table class="table table-hover">
+        <thead class="table-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">menu_name</th>
+            <!-- <th scope="col">detail</th> -->
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody v-for="(model, index) in models" :key="index">
+          <tr>
+            <th scope="row">
+              <i @click="updateId(model.id)" class="bi bi-arrow-up"></i
+              >{{ model.id }}
+            </th>
+            <td>{{ model.menu_name }}</td>
+            <!-- <td>{{ model.detail }}</td> -->
+            <td v-html="rawHtml"></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   head() {
     return {
@@ -50,7 +59,67 @@ export default {
       ],
     };
   },
+  data() {
+    return {
+      id: null,
+      rawHtml:
+        '<button type="button" class="btn btn-outline-warning btn-sm"><i class="bi bi-pencil-square"></i></button> <button type="button" class="btn btn-outline-danger btn-sm"><i class="bi bi-trash"></i></button>',
+      models: [
+        {
+          id: 1,
+          menu_name: "Green",
+          detail: "Goblan",
+        },
+        {
+          id: 2,
+          menu_name: "Luffy",
+          detail: "Monkey",
+        },
+        {
+          id: 3,
+          menu_name: "G",
+          detail: "Dragon",
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    updateId(value) {
+      console.log("helloworld");
+      this.id = value;
+    },
+    getData() {
+      axios
+        .get("http://localhost:3000/foods/")
+        .then((res) => {
+          console.log(res);
+          this.models = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css");
+
+/* .action-item:hover {
+  cursor: pointer;
+}
+
+.my-custom-scrollbar {
+  position: relative;
+  height: 400px;
+  overflow: auto;
+}
+
+.table-wrapper-scroll-y {
+  display: block;
+} */
+</style>
