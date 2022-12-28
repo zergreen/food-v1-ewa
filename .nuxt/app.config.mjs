10d8c31f6914264bd5ebc21718408073298e1839
zergreen <kawhao101@gmail.com>
2022-12-29 03:04:09 +0700

    rm: boostrap in package.json

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/home/homer/eam/my-project (tailwind)/food-v1-ewa/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "/home/homer/eam/my-project (tailwind)/food-v1-ewa/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
